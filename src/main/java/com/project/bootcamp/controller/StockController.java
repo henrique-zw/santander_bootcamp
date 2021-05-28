3d68d894da08725e4920679f4d42f333547349cf
henrique <henriquederlam@gmail.com>
2021-05-28 16:04:07 -0300

    Implemented  basic StockController method

---
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -0,0 +1,22 @@
+package com.project.bootcamp.controller;
+
+import com.project.bootcamp.model.dto.StockDTO;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+@RestController
+@RequestMapping(value = "/stock")
+
+public class StockController {
+    
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces =  MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+}
