d4aa0d3ac12267450b41ccca7412fdd276564a38
henrique <henriquederlam@gmail.com>
2021-05-30 15:06:40 -0300

    Update application.yml and BootcampApplication

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -14,17 +14,18 @@ import io.swagger.v3.oas.models.info.License;
 @SpringBootApplication
 public class BootcampApplication {
 
+	@Autowired
+	private BuildProperties buildProps;
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampApplication.class, args);
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI(){
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description){
 		return new OpenAPI().info(new Info()
-		.title("")
-		.version("1.0")
-		.termsOfService("http://swagger.io/term")
-		.license(new License().
-		name("Apache 2.0").url("http://springdoc.org")));
+		.title(description)
+		.version(buildProps.getVersion())
+		.termsOfService("http://swagger.io/terms/")
+		.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 }
